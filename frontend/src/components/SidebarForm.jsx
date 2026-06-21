0a3828e9668f4a74fb89e3cdd42cdd3ef4d62a05
Khadija Khalid <ksaeed191@gmail.com>
2026-06-21 17:42:49 +0500

    Connect forntend to Render backend

--- frontend/src/components/SidebarForm.jsx
+++ frontend/src/components/SidebarForm.jsx
@@ -33,18 +33,16 @@ function SidebarForm({ setPredictionData }) {
 
     e.preventDefault();
 
-    const response = await fetch(
-      "http://127.0.0.1:5000/predict",
-      {
-        method: "POST",
-
-        headers: {
-          "Content-Type": "application/json"
-        },
-
-        body: JSON.stringify(formData)
-      }
-    );
+   const response = await fetch(
+  "https://ml-flight-delay-predict.onrender.com/predict",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+);
 
     const data = await response.json();
 
